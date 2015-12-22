7d81372028bf42d9fb4a41842e5bfb229cd0e430
jeez19 <jeez1989@gmail.com>
2015-12-22 17:43:28 +0800

    重写subProc和subTable的解析。

--- src/com/zexin/procparserely/GameStart.java
+++ src/com/zexin/procparserely/GameStart.java
@@ -6,7 +6,7 @@ package com.zexin.procparserely;
 public class GameStart {
     public static void goBabyGo(String originalProc) {
         SQLstmt sqlStmt = new SQLstmt();
-        sqlStmt.strTBName = originalProc;
+        sqlStmt.strTBName = originalProc.toLowerCase();
 
         sqlStmt.strStmtDetail = SQLstmtExe.stmtParse(sqlStmt.strTBName);
 
@@ -14,44 +14,27 @@ public class GameStart {
         ProcCalledRely procCalledRely = new ProcCalledRely(sqlStmt.strTBName, sqlStmt.strStmtDetail);
         System.out.println("MainProcName is: " + procCalledRely.mainProcName);
         System.out.println("MainProcStmt is: \n" + procCalledRely.mainProcStmt);
-        System.out.print("\n");
 
-        //StringBuilder subProcSQL = new StringBuilder();
+        System.out.print("\n");
         for (String e : procCalledRely.subProc) {
             String tmpSQL = "insert into zzx_tb_proc_rely (proc_name, rely_type, rely_name) values ('" + procCalledRely.mainProcName + "','subProc','" + e + "')";
             System.out.println("        SubProc is:" + tmpSQL);
-            SQLstmtExe.executeSQL(tmpSQL);
-            //subProcSQL.append(tmpSQL);
             goBabyGo(e);
         }
 
-
         System.out.print("\n");
-        //StringBuilder intoTableSQL = new StringBuilder();
         for (String e : procCalledRely.tbIntoMap) {
             String tmpSQL = "insert into zzx_tb_proc_rely (proc_name, rely_type, rely_name) values ('" + procCalledRely.mainProcName + "','into table','" + e + "')";
             System.out.println("        SubIntoTable is:" + tmpSQL);
-            SQLstmtExe.executeSQL(tmpSQL);
-            //intoTableSQL.append(tmpSQL);
+            //SQLstmtExe.executeSQL(tmpSQL);
         }
 
         System.out.print("\n");
-        //StringBuilder fromTableSQL = new StringBuilder();
         for (String e : procCalledRely.tbFromMap) {
             String tmpSQL = "insert into zzx_tb_proc_rely (proc_name, rely_type, rely_name) values ('" + procCalledRely.mainProcName + "','from table','" + e + "')";
             System.out.println("        SubFromTable is:" + tmpSQL);
-            SQLstmtExe.executeSQL(tmpSQL);
-            //fromTableSQL.append(tmpSQL);
+            //SQLstmtExe.executeSQL(tmpSQL);
         }
-        /*
-        System.out.println(subProcSQL);
-        System.out.println(intoTableSQL);
-        System.out.println(fromTableSQL);
-        StringBuilder executeSQL = new StringBuilder();
-        executeSQL.append(subProcSQL).append(intoTableSQL).append(fromTableSQL);
-        System.out.println(executeSQL);
-        SQLstmtExe.executeSQL(executeSQL.toString());
-        */
     }
 
     public static void main(String args[]){
--- src/com/zexin/procparserely/ProcCalledRely.java
+++ src/com/zexin/procparserely/ProcCalledRely.java
@@ -1,8 +1,6 @@
 package com.zexin.procparserely;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
